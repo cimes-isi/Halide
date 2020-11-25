c5cc57dfc51a9453a2bffdd1a6ecce54435266fe
Tzu-Mao Li <tzumao@mit.edu>
2020-11-25 13:26:04 -0500

    [distributed Halide] Add a test with mixed non-distributed and distributed Func

--- test/distributed/distributed.cpp
+++ test/distributed/distributed.cpp
@@ -123,6 +123,45 @@ int main(int argc, char **argv) {
         }
     }
 
+    {
+        ImageParam in(Int(32), 1);
+        Func f;
+        f(x) = 2 * in(x) + 1;
+        Func g;
+        g(x) = f(x) + 1;
+        g.distribute(x);
+        f.compute_root();
+
+        int num_elements = 23;
+        g.infer_input_bounds({num_elements});
+        Buffer<int> in_buf = in.get();
+        int buf_min = 0;
+        int buf_max = num_elements - 1;
+        if (in_buf.dim(0).min() != buf_min) {
+            printf("rank %d: in_buf.dim(0).min() = %d instead of %d\n", rank, in_buf.dim(0).min(), buf_min);
+            MPI_Finalize();
+            return -1;
+        }
+        if (in_buf.dim(0).max() != buf_max) {
+            printf("rank %d: in_buf.dim(0).max() = %d instead of %d\n", rank, in_buf.dim(0).max(), buf_max);
+            MPI_Finalize();
+            return -1;
+        }
+        for (int x = in_buf.dim(0).min(); x <= in_buf.dim(0).max(); x++) {
+            in_buf(x) = 2 * x;
+        }
+
+        Buffer<int> out = g.realize(num_elements);
+        for (int x = out.dim(0).min(); x <= out.dim(0).max(); x++) {
+            int correct = 4 * x + 2;
+            if (out(x) != correct) {
+                printf("out(%d) = %d instead of %d\n", x, out(x), correct);
+                MPI_Finalize();
+                return -1;
+            }
+        }
+    }
+
     MPI_Finalize();
     return 0;
 }
