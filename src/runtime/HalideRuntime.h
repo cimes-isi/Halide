5dd9d876e3f8a2c81dd806807445ba93f27f1847
Tzu-Mao Li <tzumao@mit.edu>
2020-12-10 14:16:48 -0500

    [distributed Halide] Fix CMake

--- src/runtime/HalideRuntime.h
+++ src/runtime/HalideRuntime.h
@@ -1440,8 +1440,8 @@ typedef struct halide_buffer_t {
      *  this buffer is not allocated in a distributed setting. */
     halide_dimension_t *distributed_global_dim;
 
-    /** Pads the buffer up to a multiple of 8 bytes */
-    void *padding;
+    // The buffer is designed to be a multiple of 8 bytes on both 32-bit and 64-bit machines.
+    // Add padding if necessary to keep it this way.
 
 #if (__cplusplus >= 201103L || _MSVC_LANG >= 201103L)
     /** Convenience methods for accessing the flags */
