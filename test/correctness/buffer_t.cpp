56b28c429c012255eb09c26d3194c97086a63b02
Tzu-Mao Li <tzumao@mit.edu>
2020-12-10 09:54:35 -0500

    [distributed Halide] fix buffer test

--- test/correctness/buffer_t.cpp
+++ test/correctness/buffer_t.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
     CHECK(type, 24, 32);
     CHECK(dimensions, 28, 36);
     CHECK(dim, 32, 40);
-    CHECK(padding, 36, 48);
+    CHECK(distributed_global_dim, 36, 48);
 
     static_assert((sizeof(void *) == 8 && sizeof(halide_buffer_t) == 56) ||
                       (sizeof(void *) == 4 && sizeof(halide_buffer_t) == 40),
