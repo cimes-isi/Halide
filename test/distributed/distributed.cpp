61569b18ab85b658502f5311ed4e20802bffda67
Tzu-Mao Li <tzumao@mit.edu>
2021-01-20 08:35:26 -0500

    update stage test

--- test/distributed/distributed.cpp
+++ test/distributed/distributed.cpp
@@ -246,6 +246,44 @@ int main(int argc, char **argv) {
         }
     }
 
+    {
+        ImageParam in(Int(32), 1);
+        Func f;
+        f(x) = 2 * in(x) + 1;
+        f(x) = f(x) + 1;
+        f.distribute(x);
+
+        int num_elements = 23;
+        f.infer_input_bounds({num_elements});
+        Buffer<int> in_buf = in.get();
+        int num_elements_per_proc = (num_elements + numprocs - 1) / numprocs;
+        int buf_min = num_elements_per_proc * rank;
+        int buf_max = std::min(buf_min + num_elements_per_proc - 1, num_elements - 1);
+        if (in_buf.dim(0).min() != buf_min) {
+            printf("rank %d: in_buf.dim(0).min() = %d instead of %d\n", rank, in_buf.dim(0).min(), buf_min);
+            MPI_Finalize();
+            return -1;
+        }
+        if (in_buf.dim(0).max() != buf_max) {
+            printf("rank %d: in_buf.dim(0).max() = %d instead of %d\n", rank, in_buf.dim(0).max(), buf_max);
+            MPI_Finalize();
+            return -1;
+        }
+        for (int x = in_buf.dim(0).min(); x <= in_buf.dim(0).max(); x++) {
+            in_buf(x) = 2 * x;
+        }
+
+        Buffer<int> out = f.realize(num_elements);
+        for (int x = out.dim(0).min(); x <= out.dim(0).max(); x++) {
+            int correct = 4 * x + 2;
+            if (out(x) != correct) {
+                printf("out(%d) = %d instead of %d\n", x, out(x), correct);
+                MPI_Finalize();
+                return -1;
+            }
+        }
+    }
+
     MPI_Finalize();
     return 0;
 }
