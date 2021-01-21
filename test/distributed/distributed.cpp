c5f11eed4531e7519a69be6c3943bb90fb93461c
Tzu-Mao Li <tzumao@mit.edu>
2021-01-21 07:58:29 -0500

    [distributed Halide] add split test

--- test/distributed/distributed.cpp
+++ test/distributed/distributed.cpp
@@ -252,6 +252,7 @@ int main(int argc, char **argv) {
         f(x) = 2 * in(x) + 1;
         f(x) = f(x) + 1;
         f.distribute(x);
+        f.update().distribute(x);
 
         int num_elements = 23;
         f.infer_input_bounds({num_elements});
@@ -274,8 +275,60 @@ int main(int argc, char **argv) {
         }
 
         Buffer<int> out = f.realize(num_elements);
+        if (out.dim(0).min() != buf_min) {
+            printf("rank %d: out.dim(0).min() = %d instead of %d\n", rank, out.dim(0).min(), buf_min);
+            MPI_Finalize();
+            return -1;
+        }
+        if (out.dim(0).max() != buf_max) {
+            printf("rank %d: out.dim(0).max() = %d instead of %d\n", rank, out.dim(0).max(), buf_max);
+            MPI_Finalize();
+            return -1;
+        }
         for (int x = out.dim(0).min(); x <= out.dim(0).max(); x++) {
             int correct = 4 * x + 2;
+            if (out(x) != correct) {
+                printf("out(%d) = %d instead of %d\n", x, out(x), correct);
+                MPI_Finalize();
+                return -1;
+            }
+        }
+    }
+
+    {
+        ImageParam in(Int(32), 1, "in");
+        Func f("f");
+        f(x) = 2 * in(x) + 1;
+        Var xo, xi;
+        f.compute_root()
+         .split(x, xo, xi, 64);
+        f.distribute(xo);
+
+        int num_elements = 16383;
+        Buffer<int> in_buf(nullptr, 0);
+        in_buf.set_distributed(std::vector<int>{num_elements});
+        f.infer_input_bounds({num_elements}, get_jit_target_from_environment(), {{in, &in_buf}});
+        in.set(in_buf);
+        int num_elements_per_proc = (num_elements + numprocs - 1) / numprocs;
+        int buf_min = num_elements_per_proc * rank;
+        int buf_max = std::min(buf_min + num_elements_per_proc - 1, num_elements - 1);
+        if (in_buf.dim(0).min() != buf_min) {
+            printf("rank %d: in_buf.dim(0).min() = %d instead of %d\n", rank, in_buf.dim(0).min(), buf_min);
+            MPI_Finalize();
+            return -1;
+        }
+        if (in_buf.dim(0).max() != buf_max) {
+            printf("rank %d: in_buf.dim(0).max() = %d instead of %d\n", rank, in_buf.dim(0).max(), buf_max);
+            MPI_Finalize();
+            return -1;
+        }
+        for (int x = in_buf.dim(0).min(); x <= in_buf.dim(0).max(); x++) {
+            in_buf(x) = 2 * x;
+        }
+
+        Buffer<int> out = f.realize(num_elements);
+        for (int x = out.dim(0).min(); x <= out.dim(0).max(); x++) {
+            int correct = 4 * x + 1;
             if (out(x) != correct) {
                 printf("out(%d) = %d instead of %d\n", x, out(x), correct);
                 MPI_Finalize();
